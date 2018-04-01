4e2f527cf9d85348b9af3374c05e1d694ad56f87
lipeng <327263458@qq.com>
2018-04-01 15:13:10 +0800

    express and  MongoDB

---
+++ server/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// 链接mongo 并且使用nexus 集合
+const DB_URL = 'mongodb://localhost:27017/nexus';
+mongoose.connect(DB_URL);
+mongoose.connection.on('connected', function () {
+    console.log('mongo conect success');
+})
+// 类似于mysql的表,mongol里有模型
+const User = mongoose.model('user', new mongoose.Schema({
+    user: {
+        type: String,
+        require: true
+    },
+    age: {
+        type: Number,
+        require: true
+    }
+}))
+// // 新增数据
+// User.create({
+//     user: 'xiaobaai',
+//     age: 16
+// }, function (err, doc) {
+//     if (!err) {
+//         console.log(doc)
+//     } else {
+//         console.log(err);
+//     }
+// })
+
+// 删除数据
+// User.remove({age:16},function(err,doc){
+//     console.log(doc)
+// })
+
+// 修改数据
+User.update({
+    'user': 'xiaobai'
+}, {
+    '$set': {
+        age: 26
+    }
+}, function (err, doc) {
+    console.log(doc)
+})
+
+//新建app
+const app = express();
+app.get('/', function (req, res) {
+    res.send('<h1>Hello world</h1>')
+})
+// 返回JSON
+app.get('/data', function (req, res) {
+    // 查找
+    User.find({}, function (err, doc) {
+        res.json(doc)
+    })
+})
+
+app.listen(8086, function () {
+    console.log('Node app start at port 8086');
+})
